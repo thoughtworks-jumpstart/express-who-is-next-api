46d03a403fe4b9b12f1e8378ad48a26ffc21430a
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-23 10:51:39 +0800

    [mabel] Add GET / endpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send({
+    "0": "GET    /",
+    "1": "POST   /whonext",
+    "2": "GET    /whonext",
+    "3": "-----------------------",
+    "4": "GET    /participants",
+    "5": "POST   /participants",
+    "6": "GET /participants/:id",
+    "7": "PUT /participants/:id",
+    "8": "DELETE /participants/:id",
+  });
+});
+
+app.post("whonext", (req, res) => {
+  res.send([{ id: 1, name: "xxx" }]);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Express app started on http://localhost:${PORT}`);
+});
