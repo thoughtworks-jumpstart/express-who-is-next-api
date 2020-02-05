bfc38e01dfd82605299012003366cb61b37872e9
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-02-05 15:32:20 +0800

    [mabel] Use process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT = 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || PORT, () => {
   console.log(`Express app started on http://localhost:${PORT}`);
 });
